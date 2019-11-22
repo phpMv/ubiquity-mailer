7b960db3feba42aff48f1d58339933946237a25d
jcheron <myaddressmail@gmail.com>
2019-11-22 15:03:55 +0100

    fix cs

--- src/Ubiquity/mailer/MailerManager.php
+++ src/Ubiquity/mailer/MailerManager.php
@@ -8,62 +8,70 @@ use Ubiquity\utils\base\UFileSystem;
 /**
  * Ubiquity\mailer$MailerManager
  * This class is part of Ubiquity
+ *
  * @author jcheron <myaddressmail@gmail.com>
  * @version 1.0.0
  *
  */
 class MailerManager {
-	
+
 	/**
+	 *
 	 * @var PHPMailer
 	 */
 	private static $mailer;
-	
+
 	private static $config;
-	
-	private static $dConfig=['host'=>'127.0.0.1','port'=>587,'auth'=>false,'user'=>'','password'=>'','protocol'=>'smtp'];
-	
-	private static function getConfigPath(){
-		return \ROOT.\DS.'config'.\DS.'mailer.php';
+
+	private static $dConfig = [
+		'host' => '127.0.0.1',
+		'port' => 587,
+		'auth' => false,
+		'user' => '',
+		'password' => '',
+		'protocol' => 'smtp'
+	];
+
+	private static function getConfigPath() {
+		return \ROOT . \DS . 'config' . \DS . 'mailer.php';
 	}
-	
+
 	/**
 	 * Start the mailer manager.
 	 */
-	public static function start(){
-		$mailer=new PHPMailer();
-		$config=self::loadConfig();
-		$mailer->Host=$config['host'];
-		$mailer->Port=$config['port'];
-		$mailer->Mailer=$config['protocol'];
-		if($config[protocol]==='smtp'){
-			if($config['auth']){
-				$mailer->Password=$config['password'];
-				$mailer->Username=$config['user'];
+	public static function start() {
+		$mailer = new PHPMailer();
+		$config = self::loadConfig();
+		$mailer->Host = $config['host'];
+		$mailer->Port = $config['port'];
+		$mailer->Mailer = $config['protocol'];
+		if ($config[protocol] === 'smtp') {
+			if ($config['auth']) {
+				$mailer->Password = $config['password'];
+				$mailer->Username = $config['user'];
 			}
 		}
-		self::$mailer=$mailer;
+		self::$mailer = $mailer;
 	}
-	
-	public static function initConfig(){
-		self::saveConfig( self::$dConfig);
+
+	public static function initConfig() {
+		self::saveConfig(self::$dConfig);
 	}
-	
-	public static function saveConfig($config){
-		$content = "<?php\nreturn " . UArray::asPhpArray ( $config, 'array' ) . ';';
-		$filename = "";
+
+	public static function saveConfig($config) {
+		$content = "<?php\nreturn " . UArray::asPhpArray($config, 'array') . ';';
 		$path = self::getConfigPath();
-		if (UFileSystem::safeMkdir ( $path )) {
-			if (@\file_put_contents ( $path . \DS . $filename, $content, LOCK_EX ) === false) {
-				throw new \Exception ( "Unable to write mailer config file: {$filename}" );
+		if (UFileSystem::safeMkdir($path)) {
+			if (@\file_put_contents($path, $content, LOCK_EX) === false) {
+				throw new \Exception("Unable to write mailer config file: {$path}");
 			}
 		} else {
-			throw new \Exception ( "Unable to create folder : {$path}" );
+			throw new \Exception("Unable to create folder : {$path}");
 		}
 	}
-	
-	public static function loadConfig(){
-		return self::$config=\array_merge(include self::getConfigPath(),self::$dConfig);
+
+	public static function loadConfig() {
+		return self::$config = \array_merge(include self::getConfigPath(), self::$dConfig);
 	}
 }
 
